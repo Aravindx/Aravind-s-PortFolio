06901b590eb4af82c4bf32a6a76812457ea28aa8
Aravindx <aravindmathi517@gmail.com>
2023-07-31 13:12:11 +0530

    about andrsume update

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -11,15 +11,22 @@ function AboutCard() {
             Hi Everyone, I am <span className="purple">Aravind M </span>
             from <span className="purple"> Nagapattinam, Tamil Nadu.</span>
             <br />
-            <br></br> I'm Aravind hard-working engineering graduate specializing
-            in Biomedical Engineering. I'm a Fresher and I have 6 months of
-            experience in Guvi Institution as a MERN stack Learner.
+            <br></br> I seek to work in a competitive field and ready to accept
+            the challenges, utilizing my skills, would like to work with a
+            highly esteemed company which gives me a platform to use my
+            expertise and skills for mutual growth and benefit of company and
+            myself.
             <br />
-            I mainly work with the MERN stack but I have a profound knowledge of
-            HTML & CSS, Javascript.
+            I Started off my self-learning journey with online tutorials like
+            Google and Youtube. Then I took a step further and Enrolled in the
+            GUVI IIT MADRAS as Fullstack developer Nanodegree Program which
+            involved extensive programming and real world projects. Later, I
+            learned MERN Stack From Guvi and built an Website using React JS,
+            Node, Express Js, MongoDB.
             <br />
             <br></br>
-            Apart from coding, some other activities that I love to do!
+            Apart from technical skills I do possess some hobbies that I love to
+            do
           </p>
           <ul>
             <li className="about-activity">
@@ -27,9 +34,6 @@ function AboutCard() {
             </li>
             <li className="about-activity">
               <ImPointRight /> Exploring new places and people
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Fitness related activities
             </li>
           </ul>
 
